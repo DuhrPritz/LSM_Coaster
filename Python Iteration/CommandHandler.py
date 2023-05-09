3c78c254cfb7f2225baf7de1612b886a6904496e
Daniel Pritzker <drpritzker@gmail.com>
2023-05-09 09:53:52 -0400

    update

---
+++ Python Iteration/CommandHandler.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import Mock.GPIO as GPIO
+import time
+GPIO.setwarnings(False)
+
+#LSM asci gui
+def motor():
+def setNorth(input):
+def setSouth(input):
+def setOff(input):
+def pulse(input):
+def getCmd(input):
--- Python Iteration/MagnetHandler.py
+++ Python Iteration/MagnetHandler.py
@@ -1,2 +1,13 @@
 #https://pypi.org/project/pcf8574-io/
+#!/usr/bin/env python3
+import Mock.GPIO as GPIO
+import time
+GPIO.setwarnings(False)
+
+#LSM asci gui
+def motor():
+def setNorth():
+def setSouth():
+def setOff():
+def pulse():
 
