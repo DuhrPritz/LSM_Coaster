9b9bd2114249423771e53829f9c1e117fe2817ce
Daniel Pritzker <drpritzker@gmail.com>
2023-05-10 14:01:04 -0400

    set project to micropy for library access

--- Python Iteration/MagnetHandler.py
+++ Python Iteration/MagnetHandler.py
@@ -1,10 +1,12 @@
-#https://pypi.org/project/pcf8574-io/
+#https://github.com/2kofawsome/pi74HC595
 #!/usr/bin/env python3
 import Mock.GPIO as GPIO
 import time
-import pcf8574_io
-GPIO.setwarnings(False)
-
+from pi74HC595 import pi74HC595
+GPIO.setmode(gpio.BOARD)
+#GPIO.setwarnings(False)
+shifty = pi74HC595(7, 37, 22, 5)
+shiftState[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,]
 #LSM asci gui
 def motor():
 def setNorth():
